1b94014f0742555edc6df90e9162556d81061782
koppelit101424-art <koppelit101424@gmail.com>
2026-03-28 10:19:46 +0800

    delete old

--- old version/ITSM/admin/user/includes/toggle_status.php
+++
@@ -1,4 +0,0 @@
-    <form id="toggleStatusForm" method="POST" style="display:none;">
-        <input type="hidden" name="toggle_status_user_id" id="toggleStatusUserId">
-        <input type="hidden" name="new_status" id="toggleStatusValue">
-    </form>
--- old version/v1/AMS_PHP/admin/README.md
+++
@@ -1,2 +0,0 @@
-# IT-Inventory-Management
-IT Inventory Management
