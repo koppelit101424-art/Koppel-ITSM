bde9253748cfe640e182698e63bfafab79ddeb49
koppelit101424-art <koppelit101424@gmail.com>
2026-04-01 13:26:45 +0800

    user sidebar

--- ITSM/user/sidebar.php
+++ ITSM/user/sidebar.php
@@ -48,58 +48,18 @@ System
 <span class="menu-text">Issued Asset</span>
 </a>
 
-<!-- INVENTORY -->
-<!-- <div class="menu-group">
-
-<a data-title="Inventory" class="dropdown-btn <?= in_array($page,$inventory_pages) ? 'active' : '' ?>">
-<i class="bi bi-box-seam"></i>
-<span class="menu-text">Inventory</span>
-<i class="bi bi-chevron-down ms-auto dropdown-arrow"></i>
-</a>
-
-<div class="submenu <?= in_array($page,$inventory_pages) ? 'show' : '' ?>">
-
-<a data-title="All Assets" class="<?= ($page=='inventory/all_assets') ? 'active' : '' ?>" href="?page=inventory/all_assets">
-<i class="bi bi-list"></i>
-<span class="menu-text">All Assets</span>
-</a>
-
-<a data-title="Desktops" class="<?= ($page=='inventory/desktops') ? 'active' : '' ?>" href="?page=inventory/desktops">
-<i class="bi bi-pc-display"></i>
-<span class="menu-text">Desktops</span>
-</a>
-
-<a data-title="Laptops" class="<?= ($page=='inventory/laptops') ? 'active' : '' ?>" href="?page=inventory/laptops">
-<i class="bi bi-laptop"></i>
-<span class="menu-text">Laptops</span>
-</a>
-
-</div>
-</div> -->
-
 <!-- TICKETS -->
-<div class="menu-group">
-
-<a data-title="Tickets" class="dropdown-btn <?= in_array($page,$tickets_pages) ? 'active' : '' ?>">
-<i class="bi bi-ticket"></i>
-<span class="menu-text">Tickets</span>
-<i class="bi bi-chevron-down ms-auto dropdown-arrow"></i>
-</a>
-
-<div class="submenu <?= in_array($page,$tickets_pages) ? 'show' : '' ?>">
 
 <a data-title="All Tickets" class="<?= ($page=='ticket/all_tickets') ? 'active' : '' ?>" href="?page=ticket/all_tickets">
 <i class="bi bi-list"></i>
-<span class="menu-text">All Tickets</span>
+<span class="menu-text">Tickets</span>
 </a>
+
 
 <a data-title="Requests" class="<?= ($page=='ticket/requests') ? 'active' : '' ?>" href="?page=ticket/requests">
 <i class="bi bi-file-text"></i>
 <span class="menu-text">Requests</span>
 </a>
-
-</div>
-</div> 
 
 <div class="sidebar-footer">
 <a class="text-danger" href="logout.php" >
