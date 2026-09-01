ccf41bcbce4b88e0cb24d94852a114a832e4fc17
koppelit101424-art <koppelit101424@gmail.com>
2026-09-02 07:46:49 +0800

    Change database password in db.php
    Updated database password for security reasons.

--- ITSM/includes/db.php
+++ ITSM/includes/db.php
@@ -1,8 +1,14 @@
 <?php
     // Database connection
+    // $host = "localhost";
+    // $user = "root";
+    // $pass = "";
+    // $db   = "inventory_db";
+
+    // Database connection
     $host = "localhost";
     $user = "root";
-    $pass = "";
+    $pass = "Koppels3cur1ty";
     $db   = "inventory_db";
 
     $conn = new mysqli($host, $user, $pass, $db);
