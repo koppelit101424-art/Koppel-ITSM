06a5bc132ede6b5196da6ebc11ffe26b654625dd
koppelit101424-art <koppelit101424@gmail.com>
2026-09-02 08:44:43 +0800

    Restore database connection parameters

--- ITSM/includes/db.php
+++ ITSM/includes/db.php
@@ -1,15 +1,14 @@
 <?php
-    // Database connection
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db   = "inventory_db";
-
-    // Database connection
     // $host = "localhost";
     // $user = "root";
-    // $pass = "Koppels3cur1ty";
+    // $pass = "";
     // $db   = "inventory_db";
+
+    Database connection
+    $host = "localhost";
+    $user = "root";
+    $pass = "Koppels3cur1ty";
+    $db   = "inventory_db";
 
     $conn = new mysqli($host, $user, $pass, $db);
 
